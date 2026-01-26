c8601985c8014b57434dd0e2d751544e9210a012
Harvey Duong <hduongd@gmail.com>
2026-01-26 16:19:34 -0600

    Start refactoring checkMove

--- othello.js
+++ othello.js
@@ -24,7 +24,31 @@ var testBoard = [
      "-", "W", "W", "B", "W", "W", "-", "-",
      "-", "-", "-", "-", "-", "-", "-", "-",
      "-", "-", "-", "-", "-", "-", "-", "-",
-     "-", "-", "B", "B", "W", "B", "W", "-",]
+     "-", "-", "B", "B", "W", "B", "W", "-",],
+    ["-", "-", "-", "-", "-", "-", "B", "-",
+     "-", "-", "-", "-", "-", "-", "-", "W",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "W", "B", "-", "-", "-",
+     "-", "-", "-", "B", "W", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",],
+    ["-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "W",
+     "B", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",],
+    ["-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "B",
+     "W", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",
+     "-", "-", "-", "-", "-", "-", "-", "-",],
 ];
 
 function initBoard() {
@@ -76,6 +100,11 @@ function algebraicToIdx(algebraic) {
     return 8*(Number(row)-1) + (col.charCodeAt(0) - 'a'.charCodeAt(0));
 }
 
+function positionToRowAndCol(position) {
+    // [row, column]
+    return [Math.floor(position / 8), position % 8]
+}
+
 /*
 Legal moves for black, move 1
 a b c d e f g h
@@ -93,6 +122,57 @@ CONDITIONS for legal move:
 2. there must be an unbroken sequence of opposite colored pieces in between,
    ending with a piece of the same color, and no empty squares in between
 */
+
+function coordinateToPosition(coordinate) {
+    return coordinate[0]*8 + coordinate[1];
+}
+function checkMoveByCoordinates(coordinate) {
+    var leftLegal = false;
+    var upRightDiagonalLegal = false;
+
+    // leftwards check
+    var row = coordinate[0];
+    var col = coordinate[1];
+    var position = coordinateToPosition(coordinate);
+
+    var currPosLeft = col - 1;
+    var currPosUpperRight = [row-1, col+1];
+    if (board[position] !== "-") {
+        return false;
+    }
+    if (blackToMove) {
+        //if (currPosLeft > -1 && board[posLeft] !== "W") { //LEFT
+        if (currPosLeft > -1 && board[coordinateToPosition([row,currPosLeft])] !== "W") { //LEFT
+            // the left piece is not a white piece, so the left side is not legal
+            leftLegal = false;
+        } else {
+            // otherwise, the left side might be legal, so check
+            while (currPosLeft > -1) {
+                if (board[coordinateToPosition([row,currPosLeft])] === "B")
+                    leftLegal = true;
+                else if (board[coordinateToPosition([row,currPosLeft])] === "-")
+                    break;
+                currPosLeft--;
+            }
+        }
+
+        if (currPosUpperRight[0] > -1 && currPosUpperRight[1] < 8
+            && board[coordinateToPosition(currPosUpperRight)] !== "W" ) {
+                upRightDiagonalLegal = false;
+        } else {
+            // otherwise, the left side might be legal, so check
+            while (currPosUpperRight[0] > -1 && currPosUpperRight[1] < 8) {
+                if (board[coordinateToPosition(currPosUpperRight)] === "B")
+                    leftLegal = true;
+                else if (board[coordinateToPosition(currPosUpperRight)] === "-")
+                    break;
+                currPosUpperRight[0]--;
+                currPosUpperRight[1]++;
+            }
+        }
+    }
+    return leftLegal || upRightDiagonalLegal;
+}
 function checkMove(position){
     //is move legal
     //what pieces flipped
@@ -152,7 +232,7 @@ function checkMove(position){
         if (currPosRight < 9 && board[posRight] !== "W") { //RIGHT
             rightLegal = false;
         } else {
-            while (currPosRight < 9) {
+            while (currPosRight < 8) {
                 if (board[posRight] === "B")
                     rightLegal = true;
                 else if (board[posRight] === "-")
@@ -199,7 +279,8 @@ function highlightMove(){
         var output = ""
         for (var c = 0; c < COLUMNS; c++) {
             var idx = 8*r + c;
-            if (checkMove(idx)){
+            //if (checkMove(idx)){
+            if (checkMoveByCoordinates([r,c])){
                 output += "* ";
             } else {
                 output += board[idx] + " ";
@@ -211,7 +292,7 @@ function highlightMove(){
 
 function checkMoveAlgebraic(position) {
     var idx = algebraicToIdx(position);
-    return checkMove(idx);
+    return checkMove(idx);// TODO
 }
 
 function makeMove(position) {
@@ -235,4 +316,37 @@ function test() {
     }
 }
 
-test()
+function randomTest() {
+    board = Array.from({length: 64}, () => {
+        var x = Math.random();
+        if (x < 0.5) return "-"
+        else if (x < 0.75) return "W"
+        else return "B"
+    })
+    highlightMove(board);
+}
+
+function test2() {
+    board = Array.from({length: 64});
+    var i = 0;
+    while (i < 64) {
+        board[i] = "X";
+        i += 9;
+    }
+    print(board);
+}
+
+function main() {
+    test();
+    for (var i = 0; i != 10; ++i) {
+        randomTest();
+        console.log("")
+    }
+       /*
+    var positions = ["a1", "b2", "c3", "d4", "e5", "f6", "g7", "h8"]
+    for (var i = 0; i != positions.length; ++i) {
+        console.log(positionToRowAndCol(algebraicToIdx(positions[i])));
+    }
+        */
+}
+main();
